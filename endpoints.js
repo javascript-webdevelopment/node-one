5d682013e5954c2680c17f4e0cdc30517a54fb26
tstokes9328 <taytestokes9328@gmail.com>
2019-08-08 18:06:41 -0600

    added endpoint example

--- endpoints.js
+++ endpoints.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const app = express();
 
+const users = [
+    {
+        id: 1,
+        name: 'Tayte',
+        height: 6
+    },
+    {
+        id: 2,
+        name: 'Kyle',
+        height: 5.5
+    }
+];
+
 // Endpoints
+app.get('/api/users', (req, res) => {
+    res.status(200).send(users);
+})
 
 
 app.listen(3005, () => {
